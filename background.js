7aba603dc8284f4eabb7492e2b7821c2f9582894
Elvair <elvair@mountain>
2018-09-03 15:36:22 +0200

    creo los primeros archivos html js y css y creo bucle para realizar el tablero

---
+++ background.js
@@ -0,0 +1,12 @@
+//window.onload = createTable;
+
+var board = new arrayBi (7); 
+
+function createTable() {
+    var 
+    for (var i=0; i<7; i++) {
+        for (var j=0; j<7; j++){
+            board [i][j] = 0;
+        }
+    }
+}
