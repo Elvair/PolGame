81b0765cd3e14a813fe540473a2ac94a1bb13215
Elvair <elvair@mountain>
2018-09-03 22:04:40 +0200

    haciendo pruebas, en construcción

--- background.js
+++ background.js
@@ -1,13 +1,25 @@
-$(document).ready = createTable();
+// $(document).ready (createTable();
 
-var board = document.getElementById("board");
-var arrayBi = []; 
+$(document).ready(function () {
+    createTable();
+    var board = document.getElementById("board");
 
-function createTable(arrayBi) {
-    for (var i=0; i<7; i++) {       //rows
-        for (var j=0; j<7; j++){    //columns    
-            board[i][j];
+    console.log('board fuera: ', board)
+});
+
+console.log('pepe')
+
+function createTable() {
+    var arrayBi = new Array (7);
+
+    for (var i = 0; i < 7; i++) { 
+        arrayBi[i] = new Array (7);      //rows
+        for (var j = 0; j < 7; j++) {
+            console.log('board dentro: ', arrayBi);  //columns    
+            arrayBi[i];
         }
     }
+console.log (arrayBi);
+
 }
 $("#board").append("<div class=spaceBoard></div>");
